e58e8b39a499588b3603af519304015a6f462794
Jose Plana <jplana@tuenti.com>
2020-10-05 20:03:15 +0200

    Add base haproxy formula

---
+++ Formula/haproxy.rb
@@ -0,0 +1,70 @@
+class Haproxy < Formula
+  desc "Reliable, high performance TCP/HTTP load balancer"
+  homepage "https://www.haproxy.org/"
+  url "https://www.haproxy.org/download/2.2/src/haproxy-2.2.4.tar.gz"
+  sha256 "87a4d9d4ff8dc3094cb61bbed4a8eed2c40b5ac47b9604daebaf036d7b541be2"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?haproxy[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    cellar :any
+    sha256 "6da955b5869c06ee532461b103279ad76423ad30f65f572d07d4bb5e7fb079ab" => :catalina
+    sha256 "1c0481c0284c521cd9a8565e062549101721f4766a0ef193d627112aaf60cbaa" => :mojave
+    sha256 "0e1ac863874c523288947e2eacc7bca492ab8aa7406d26b6883651e16ff42ade" => :high_sierra
+  end
+
+  depends_on "openssl@1.1"
+  depends_on "pcre"
+
+  def install
+    args = %w[
+      TARGET=generic
+      USE_KQUEUE=1
+      USE_POLL=1
+      USE_PCRE=1
+      USE_OPENSSL=1
+      USE_THREAD=1
+      USE_ZLIB=1
+      ADDLIB=-lcrypto
+    ]
+
+    # We build generic since the Makefile.osx doesn't appear to work
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "LDFLAGS=#{ENV.ldflags}", *args
+    man1.install "doc/haproxy.1"
+    bin.install "haproxy"
+  end
+
+  plist_options manual: "haproxy -f #{HOMEBREW_PREFIX}/etc/haproxy.cfg"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>KeepAlive</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/haproxy</string>
+            <string>-f</string>
+            <string>#{etc}/haproxy.cfg</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/haproxy.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/haproxy.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system bin/"haproxy", "-v"
+  end
+end
